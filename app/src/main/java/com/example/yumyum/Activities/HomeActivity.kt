b35fd831cdb3caab14f07eb84d14b03d5b654f2a
xMISSJ <xmissj96@gmail.com>
2019-12-31 04:13:09 +0100

    Changes: Few layout changes. Need to figure out how to switch from fragments without toolbar...

--- app/src/main/java/com/example/yumyum/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/yumyum/Activities/HomeActivity.kt
@@ -1,13 +1,24 @@
 package com.example.yumyum.Activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.yumyum.R
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.yumyum.Fragments.HomeFragment
+import com.example.yumyum.Fragments.RatingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.default_toolbar.*
+import com.example.yumyum.R
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView;
+    private lateinit var selectedFragment: Fragment;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,10 @@ class HomeActivity : AppCompatActivity() {
         btnAddRecipe.setOnClickListener {
             onAddClick();
         }
+
+        bottomNavigation = findViewById(R.id.nvBottom);
+
+        initNavigation();
     }
 
     private fun onProfileImageClick() {
@@ -39,4 +54,25 @@ class HomeActivity : AppCompatActivity() {
         // Animation to fade into the AddActivity.
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
+
+    private fun initNavigation() {
+        // The NavController.
+        val navController = findNavController(R.id.navHostFragment);
+
+        // Connect the NavController with the BottomNavigationView.
+        NavigationUI.setupWithNavController(nvBottom, navController);
+
+        // Connect the navHostFragment with the Toolbar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph);
+
+        // Add a Destination Changed Listener. This gets called whenever the navigation controller is navigating to another fragment.
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.homeFragment -> HomeFragment();
+                R.id.rateFragment -> RatingFragment();
+            }
+        }
+
+    }
 }
+
