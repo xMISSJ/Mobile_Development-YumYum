98d2d57c54161a55015eaa239830b937eeb9093a
xMISSJ <xmissj96@gmail.com>
2020-01-01 22:57:31 +0100

    Changes: Few changes in layout. Now programming the add option in the RecyclerView of the ingredient tab.

--- app/src/main/java/com/example/yumyum/Activities/IngredientsActivity.kt
+++ app/src/main/java/com/example/yumyum/Activities/IngredientsActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yumyum.Ingredient.Ingredient
+import com.example.yumyum.Ingredient.IngredientsAdapter
 import com.example.yumyum.R
 import kotlinx.android.synthetic.main.activity_ingredients.*
 
@@ -14,9 +19,15 @@ class IngredientsActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar;
     private lateinit var view : View;
 
+    private val ingredientList = arrayListOf<Ingredient>();
+    private val ingredientAdapter = IngredientsAdapter(ingredientList);
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+
+        setToolbar();
+        initViews();
 
         btnDone.setOnClickListener {
             onClick();
@@ -25,8 +36,20 @@ class IngredientsActivity : AppCompatActivity() {
         btnAddIngredient.setOnClickListener {
             onIngredientAdd();
         }
+    }
 
-        setToolbar();
+    private fun onIngredientAdd() {
+        // Check whether the input is not blank.
+        if (etAddIngredient.text.toString().isNotBlank()){
+
+        }
+    }
+
+    private fun initViews() {
+        rvIngredients.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        rvIngredients.adapter = ingredientAdapter;
+
+        createItemTouchHelper().attachToRecyclerView(rvIngredients);
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -56,7 +79,36 @@ class IngredientsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
 
-    private fun onIngredientAdd() {
+    /**
+     * Create a touch helper to recognize when a user swipes an item from a recycler view.
+     * An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+     * and uses callbacks to signal when a user is performing these actions.
+     */
+    private fun createItemTouchHelper(): ItemTouchHelper {
 
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                // Delete ingredient at this position.
+                ingredientList.removeAt(position);
+                ingredientAdapter.notifyDataSetChanged();
+            }
+        }
+        return ItemTouchHelper(callback)
     }
+
 }
